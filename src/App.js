553578cc4e6249f59c58b100942107fc86d78327
marcelo-de-santana <marcelondesantana@outlook.com>
2023-03-24 00:56:34 -0300

    Alteração da Lista de Barbeiros

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Text ,View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from './pages/Login';
@@ -13,7 +12,7 @@ export default function App(){
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Login" component={Login} options={{headerShown:false}}/>
-        <Stack.Screen name="SignUp" component={SignUp} options={{title:'Cadastre-se',headerStyle:{backgroundColor:"#423e3c"},headerTintColor:'#ffffff'}}/>
+        <Stack.Screen name="SignUp" component={SignUp} options={{title:'Cadastre-se',headerStyle:{backgroundColor:"#3c4659"},headerTintColor:'#ffffff'}}/>
         <Stack.Screen name="Schedule" component={Schedule} options={{headerShown:false}}/>
       </Stack.Navigator>
     </NavigationContainer>
