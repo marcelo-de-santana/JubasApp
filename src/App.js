04c892698b8d9686c6cca404a9d0d75edaa6f45d
marcelo-de-santana-hp-mini <marcelondesantana@outlook.com>
2023-03-30 17:47:01 -0300

    Criação de Novas Telas e estilização do Drawer Navigation

--- src/App.js
+++ src/App.js
@@ -7,22 +7,45 @@ import 'react-native-gesture-handler';
 import Login from './pages/Login';
 import Schedule from './pages/Schedule';
 import SignUp from './pages/SignUp';
+import About from './pages/About';
 
 const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
 
 function MyDrawer() {
   return (
-    <Drawer.Navigator initialRouteName='Home' screenOptions={{ drawerStyle: { backgroundColor: '#f2f2f2' } }}>
-      <Drawer.Screen name="Home" component={Schedule} options={{
-        headerTitle: 'Jubas Barbearia',
-        headerTintColor: '#ffffff',
-        headerStyle: {
-          backgroundColor: '#3c4659',
-        },
-        headerTitleAlign:'center',
-      }} />
-      <Drawer.Screen name="Login" component={Login} />
+    <Drawer.Navigator
+    	initialRouteName='Home'
+    	screenOptions={{
+    		drawerActiveTintColor:'#ffffff',
+    		drawerActiveBackgroundColor:'#9ba7bf',
+    		drawerInactiveTintColor:'#ffffff',
+    		drawerStyle:{
+    			backgroundColor: '#3c4659',
+    			
+    		}
+    	}}
+    >
+      <Drawer.Screen name="Home" component={Schedule}
+      	options={{
+        	headerTitle: 'Jubas Barbearia',
+        	headerTintColor: '#ffffff',
+        	headerStyle: {
+          		backgroundColor: '#3c4659',
+        	},
+        	headerTitleAlign:'center',
+      	}}
+      />
+      	<Drawer.Screen name="Minha conta" component={Login} />
+	<Drawer.Screen name="Minhas compras" component={Login} />
+	<Drawer.Screen name="Promoções" component={Login} />
+	<Drawer.Screen name="Tabela de preços" component={Login} />
+	<Drawer.Screen name="Gerenciar agenda" component={Login} />
+	<Drawer.Screen name="Gerenciar barbeiros" component={Login} />
+	<Drawer.Screen name="Gerenciar pagamentos" component={Login} />
+	<Drawer.Screen name="Gerenciar estoque" component={Login} />
+	<Drawer.Screen name="Acompanhar devolução" component={Login} />
+	<Drawer.Screen name="Sobre" component={About} />
     </Drawer.Navigator>
   )
 }
@@ -32,7 +55,13 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
-        <Stack.Screen name="SignUp" component={SignUp} options={{ title: 'Cadastre-se', headerStyle: { backgroundColor: "#3c4659" }, headerTintColor: '#ffffff' }} />
+        <Stack.Screen name="SignUp" component={SignUp}
+        	options={{
+        		title: 'Cadastre-se',
+        		headerStyle: {
+        			backgroundColor: "#3c4659"
+        		},
+        		headerTintColor: '#ffffff' }} />
         <Stack.Screen name="Schedule" component={MyDrawer} options={{ headerShown: false }} />
       </Stack.Navigator>
 
